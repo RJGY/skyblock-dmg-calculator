92e202b421b7d33337a63864b6f54a31f25425d5
RJGY <rjgy2yu@gmail.com>
2021-01-30 02:30:30 +1100

    Changed ability to have just name and desc for now. Sword select has more boiler plate code.

--- src/components/ability.js
+++ src/components/ability.js
@@ -1,14 +1,9 @@
 import React from "react"
 
 class Ability extends React.Component {
-    constructor(name, damage, strength, critChance, critDamage, intelligence, ability) {
+    constructor(name, description) {
         this.name = name;
-        this.damage = damage;
-        this.strength = strength;
-        this.critChance = critChance;
-        this.critDamage = critDamage;
-        this.intelligence = intelligence;
-        this.ability = ability;
+        this.description = description;
     }
 }
 
--- src/components/sword-select.js
+++ src/components/sword-select.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from 'prop-types';
 import Sword from "sword";
+import Ability from "ability";
 // damage, strength, critChance, critDamage, intelligence, ability
 const listOfWeapons = {
     "Aspect of the Jerry" : new Sword(1,0,0,0,0,null)
@@ -8,7 +9,32 @@ const listOfWeapons = {
 
 
 class SwordSelect extends React.Component {
-    
+
+    handleSelectChange(e) {
+        setTimeout(() => {
+            this.props.onInputChange(this.state);
+        }, 50);
+    }
+
+    render() {
+        <div>
+            {Object.entries(listOfWeapons)
+                .map(([key, value]) =>
+                    <div key={key}>
+                        {value}
+                    </div>
+                )
+            }
+        </div>
+    }
+}
+
+SwordSelect.propTypes = {
+    onInputChange: PropTypes.func,
+}
+  
+SwordSelect.defaultProps = {
+    onInputChange: null,
 }
 
 export default SwordSelect;
