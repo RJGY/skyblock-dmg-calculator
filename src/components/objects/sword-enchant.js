6b0e6aae78825e6f47853f0902c928c439104642
RJGY <rjgy2yu@gmail.com>
2021-04-02 23:39:54 +1100

    Created enchants and passive objects.

--- src/components/objects/sword-enchant.js
+++ src/components/objects/sword-enchant.js
@@ -2,15 +2,19 @@
 
 import React from "react"
 
-// Passive -> Description
+// SwordEnchant -> name, currentLevel, maxLevel, damagePerLevel, damageType
 class SwordEnchant extends React.Component {
-    constructor(enchants) {
+    constructor(name, currentLevel, maxLevel, damagePerLevel, damageType) {
         super();
-        this.enchants = enchants;
+        this.name = name;
+        this.currentLevel = currentLevel;
+        this.maxLevel = maxLevel;
+        this.damagePerLevel = damagePerLevel;
+        this.damageType = damageType;
     }
 
     toString() {
-        return `Sword Enchant: \n\tDescription: ${this.enchants}`;
+        return `Sword Enchant: \n\tName: ${this.name}\n\tCurrent Level: ${this.currentLevel}\n\tMax Level: ${this.maxLevel}\n\tDamage Per Level: ${this.damagePerLevel}\n`;
     }
 }
 
--- src/components/objects/sword-passive.js
+++ src/components/objects/sword-passive.js
@@ -4,13 +4,15 @@ import React from "react"
 
 // Passive -> Description
 class SwordPassive extends React.Component {
-    constructor(description) {
+    constructor(description, damage, damageType) {
         super();
         this.description = description;
+        this.damage = damage;
+        this.damageType = damageType;
     }
 
     toString() {
-        return `Sword Passive: \n\tDescription: ${this.description}`;
+        return `Sword Passive: \n\tDescription: ${this.description}\n\tDamage: ${this.damage}\n\tDamage Type: ${this.damageType}`;
     }
 }
 
